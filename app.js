b9653f051ee944e08b40b9ba87c1abd641d8a3bb
Justin Bailey <jgbailey@gmail.com>
2020-04-28 13:53:17 -0700

    More updates.

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+// We can load a JDK type
 Java.type(`java.math.BigInteger`) 
+// On windows, we cannot load this type. 
+// On Linux, we can.
 Java.type(`org.yaml.snakeyaml.Yaml`)
 
 console.log(`success`)
